4a8a0299ce74c4e05c31bdac85f16b9cff6608aa
Alan Bañuelos <alan_blk@live.com>
2025-06-12 16:21:11 -0600

    Mitades

--- arrays/mitades.js
+++ arrays/mitades.js
@@ -6,10 +6,12 @@ Examples:
 - `numDiv(10)` should return `[5, 5]`.
 */
 
-const numeroADividir = [982]
+//! En vez de mitades, escribí código para dividir un número cualquiera entre un número lo más equitativo posible de partes
+
+const numeroADividir = (10)
 const partes = 5
 function arrayCreate(a,b){
-    const elementos=numeroADividir[0]/partes;
+    const elementos=numeroADividir/partes;
     const resultado=[""];
     for (let i = 1; i<=elementos; i++) {
         resultado.push(partes);
@@ -19,4 +21,3 @@ function arrayCreate(a,b){
 }
 let residuo=(numeroADividir%partes)
 console.log(arrayCreate(numeroADividir,partes),`El número ${numeroADividir} se divide en ${(arrayCreate(numeroADividir,partes).length)} partes iguales de ${partes} y un residuo de ${residuo}`);
-
