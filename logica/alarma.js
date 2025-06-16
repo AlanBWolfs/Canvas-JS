a78d645bb7425e38174793a8f926883155aecfd1
Alan Bañuelos <alan_blk@live.com>
2025-06-15 23:03:01 -0600

    AlarmaFinished

--- logica/alarma.js
+++ logica/alarma.js
@@ -5,21 +5,20 @@ Write a program that asks a user for the amount of seconds needed until an alarm
 Result example: "Time for bed after 10 seconds".
 */
 
-let temporizador=prompt("Introduce Los minutos y segundos que deseas que dure tu alarma");
+let temporizador=prompt("Introduce Los minutos y segundos que deseas que dure tu alarma, sepáralos con un ':'");
 
-//function alarma(tiempo){
     const minutosySegundos = temporizador.split(':');
-    console.log(minutosySegundos)
 
     const timeAlarm=[];
     for (let i = 0; i <=1; i++) {
         timeAlarm.push(parseInt(minutosySegundos[i]));
     }
-     console.log(timeAlarm)
+    
     const minutos = minutosySegundos[0];
     const segundos = minutosySegundos[1];
+    
     setTimeout(()=> {
         alert("Tiempo finalizado");},
     (minutos*60000)+(segundos*1000))
-//}
+
 
